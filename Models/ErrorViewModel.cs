fd8c709ea89c11acb2e582cc6fd0f8ae737d044f
LYO <142507365+LauraYumiOsawa@users.noreply.github.com>
2024-10-16 23:22:01 +0000

    ANDREI CONVERTER

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace HtmlPreviewApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
